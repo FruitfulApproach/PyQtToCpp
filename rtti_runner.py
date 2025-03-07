6b22a3e9391fe9268cfc2e9d748db8814822598d
FruitfulApproach <fruitfulapproach@gmail.com>
2025-03-06 16:56:49 -1000

    started rtti_runner.py

--- rtti_runner.py
+++ rtti_runner.py
@@ -1,4 +1,31 @@
 from PyQt5.QtCore import QThread
+from rtti import Rtti
+import ast
+import os
+import shutil
 
 class RttiRunner(QThread):
-    def __init__(self, parent=...)
+    def __init__(self, app_entry_filename: str, transformer: ast.NodeTransformer, parent=None):
+        super().__init__(parent)
+        self._xformer = transformer
+        self._appEntryFile = app_entry_filename
+        
+    def run(self):
+        app_entry_filename = self.entry_point_filename()        
+        xformer = self.transformer(app_entry_filename)
+        
+        with open(app_entry_filename, 'rt') as app_entry_source:
+            ast_tree = ast.parse(app_entry_source)
+            xformer.visit(ast_tree)
+        
+        exec_file = xformer.output_app_entry_filename()
+        exec_dir = os.path.dirname(exec_file)
+        exec_file = os.path.basename(exec_file)
+        os.chdir(exec_dir)
+        os.system(f'python {exec_file}')                
+        
+    def entry_point_filename(self):
+        return self._appEntryFile
+        
+    def transformer(self):
+        return self._xformer
--- rtti_transformer.py
+++ rtti_transformer.py
@@ -1,36 +1,44 @@
 import ast
-import astor
 import astunparse
 from rtti import Rtti
+import itertools
+import os
 
 class RttiTransformer(ast.NodeTransformer):
     _scope_node_types = (ast.If, ast.For, ast.While, ast.With, ast.FunctionDef, ast.ClassDef, ast.Module)
     _anon_scope_node_types = (ast.If, ast.For, ast.While, ast.With)
+    
+    def __init__(self, app_entry_filename: str):
+        super().__init__()
+        self._parents = {}
+        self._entryModuleName = app_entry_filename
         
-    def _get_siblings(self, node, include_node=True):
+    def _get_scope_siblings(self, node, include_node=True, upto_node=False):
         """
         Returns a list of sibling nodes for the given node within its parent.
         If the node has no parent or is not found within the parent's children,
         an empty list is returned.
         """
         parent = self.parent(node)        
-        siblings = []
-        for child in ast.iter_child_nodes(parent):
+        for child in self._scope_child_nodes(parent):
             if include_node or child is not node:
-                siblings.append(child)
-        return siblings    
+                yield child
+            if upto_node:
+                if child is node:
+                    break
     
-    def __init__(self, module_name=None):
-        self._parents = {}
-        self._moduleName = module_name
-        
+    def _scope_child_nodes(self, parent):
+        child_nodes = list(ast.iter_child_nodes(parent))
+        if not isinstance(parent, ast.If):
+            return child_nodes
+        return itertools.chain(child_nodes, parent.orelse)
+            
     def _count_anon_scopes_up_to_node_pos(self, node, scope_type=None):
         if scope_type is None:
             scope_type = type(node)
         count = -1
-        siblings = self._get_siblings(node)
+        siblings = self._get_scope_siblings(node, upto_node=True)
         for sibling in siblings:
-            print(sibling)
             if isinstance(sibling, scope_type):
                 count += 1
             if sibling is node:
@@ -56,10 +64,13 @@ class RttiTransformer(ast.NodeTransformer):
     def scoped_name(self, node) -> str:
         scope_str = ''               
         for outer_scope in self.scope(node):
-            #if len(scope_str) > 0:
-                #scope_str += '__'
             if isinstance(outer_scope, ast.If):
-                print("*****", outer_scope.orelse)
+                count = self._count_anon_scopes_up_to_node_pos(outer_scope)                
+                scope_str += f'{outer_scope.__class__.__name__}{count}'
+                
+                if self._has_ancestor_in(outer_scope.orelse, node):
+                    scope_str += "Else"
+                    
             elif isinstance(outer_scope, self._anon_scope_node_types):
                 count = self._count_anon_scopes_up_to_node_pos(outer_scope)                
                 scope_str += f'{outer_scope.__class__.__name__}{count}'
@@ -68,6 +79,19 @@ class RttiTransformer(ast.NodeTransformer):
             else:
                 scope_str += f'{self.ident(outer_scope)}'
         return f"{scope_str}{node.id}"
+    
+    def _has_ancestor_in(self, list_, node):
+        for x in list_:
+            if self._is_ancestor_of(node, query=x):
+                return True
+        return False
+    
+    def _is_ancestor_of(self, node, query):
+        parent = node
+        while (parent := self.parent(parent)) is not None:
+            if parent is query:
+                return True
+        return False
     
     def ident(self, node):
         if isinstance(node, ast.Module):
@@ -78,9 +102,9 @@ class RttiTransformer(ast.NodeTransformer):
             return node.name
 
     def module_name(self):
-        if self._moduleName is None:
+        if self._entryModuleName is None:
             return ""
-        return self._moduleName
+        return self._entryModuleName
         
     def visit_Assign(self, node):
         # Wraps all assignments with a custom function call
@@ -102,9 +126,15 @@ class RttiTransformer(ast.NodeTransformer):
         wrapped_value = ast.copy_location(wrapped_value, node.value)
         new_node = ast.Assign(targets=node.targets, value=wrapped_value)
         return  new_node #ast.copy_location(new_node, node)
+    
+    def output_app_entry_filename(self):
+        entry_module_name = self._entryModuleName
+        entry_module_name,_ = os.path.splitext(entry_module_name)
+        return f'{entry_module_name}_PyQt5ToCpp'
 
 
 def record_rtti(values, scoped_names):
+    # TODO put on mutex-lock
     if len(scoped_names) == 1:
         vals = [values]
     else:
@@ -116,7 +146,8 @@ def record_rtti(values, scoped_names):
     return values
 
 
-source_code = """
+if __name__ == '__main__':
+    source_code = """
 class MyClass:
     def test(self):
         x = 10
@@ -129,32 +160,36 @@ class MyClass:
             z = 11 + 2
         else:
             y = x + 6
-            
-        if x == 10:
+        if x != 10:
+            y = x + 5
             while x >= 10:
-                z = y * 2
+                y = x + 5
                 break
-        z = False
-        return z
+        elif x == 11:
+            z = 11 + 2
+        else:
+            y = x + 6            
+            
+        return x
 o = MyClass()
 o.test()
-"""
-
-# Parse the source code
-parsed_ast = ast.parse(source_code)
-
-# Transform the AST
-transformer = RttiTransformer(module_name=__name__)
-new_ast = transformer.visit(parsed_ast)
-#ast.fix_missing_locations(new_ast)
-
-#print(astor.dump_tree(new_ast))
-# Convert AST back to source code
-modified_code = astunparse.unparse(new_ast)
-print(modified_code)
-
-# Execute the modified code
-globals_dict = {'record_rtti': record_rtti, 'Rtii' : Rtti}
-exec(modified_code, globals_dict)
-
-print(Rtti.types())
+    """
+    
+    # Parse the source code
+    parsed_ast = ast.parse(source_code)
+    
+    # Transform the AST
+    transformer = RttiTransformer(app_entry_filename='rtti_xformer_test1.py')
+    new_ast = transformer.visit(parsed_ast)
+    #ast.fix_missing_locations(new_ast)
+    
+    #print(astor.dump_tree(new_ast))
+    # Convert AST back to source code
+    modified_code = astunparse.unparse(new_ast)
+    print(modified_code)
+    
+    # Execute the modified code
+    globals_dict = {'record_rtti': record_rtti, 'Rtii' : Rtti}
+    exec(modified_code, globals_dict)
+    
+    print(Rtti.types())
